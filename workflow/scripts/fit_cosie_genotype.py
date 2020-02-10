ecafa3b7cdcf26a485d949fec02535a99c0a177e
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-10 10:09:37 -0500

    getting organized

--- workflow/scripts/fit_cosie_genotype.py
+++ workflow/scripts/fit_cosie_genotype.py
@@ -1,12 +1,13 @@
-from coloc.independent_model import IndependentFactorSER 
+import pickle
 import pandas as pd
 import numpy as np
-import pickle
+from coloc.independent_model import IndependentFactorSER
+import snakemake
 
 data = pickle.load(open(snakemake.input[0], 'rb'))
 g = IndependentFactorSER(X=data['X'], Y=data['Y'], K=10)
 g.fit(max_iter=100, update_active=False, update_weights=True, update_pi=True, 
-    ARD_weights=True, update_variance=True, verbose=True)
+      ARD_weights=True, update_variance=True, verbose=True)
 path = '/'.join(snakemake.output[0].split('/')[:-1])
 name = snakemake.output[0].split('/')[-1]
 g.save(path, name)
