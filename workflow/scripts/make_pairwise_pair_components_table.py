d530a4645ee5c3fb084b269650235db76471cd4d
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-11 13:07:55 -0500

    subset data to odel

--- workflow/scripts/make_pairwise_pair_components_table.py
+++ workflow/scripts/make_pairwise_pair_components_table.py
@@ -18,17 +18,30 @@ def load_model(data, genotype_model_path=None, summary_model_path=None):
     t1 = int(path.split('/')[-1].split('_')[1])
     t2 = int(path.split('/')[-1].split('_')[3])
 
+    sub_data = {
+        'X': data['X'],
+        'LD': data['LD'],
+        'Y': data['Y'][[t1, t2]],
+        'betas': data['betas'][[t1, t2]],
+        'standard_errors': data['standard_errors'][[t1, t2]],
+        'zscores': data['zscores'][[t1, t2]],
+        'causal_snps': data['causal_snps'],
+        'true_effects': data['true_effects'][[t1, t2]],
+        'tissue_variance': data['tissue_variance'][[t1, t2]],
+        'ld' : data['ld']
+    }
+
     if genotype_model_path is not None:
         model = IndependentFactorSER(np.zeros((1, 1)), np.zeros((1, 1)), 1)
         assign(model, pickle.load(open(genotype_model_path, 'rb')))
-        model.X = data['X']
-        #model.Y = data['Y']# [[t1, t2]]
+        model.X = sub_data['X']
+        model.Y = sub_data['Y']
     else:
         model = MVNFactorSER(np.zeros((1, 1)), np.zeros((1, 1)), 1)
         assign(model, pickle.load(open(summary_model_path, 'rb')))
-        model.X = data['X']
-        #model.Y = data['zscores']# [[t1, t2]]
-    return model, t1, t2
+        model.X = sub_data['X']
+        model.Y = sub_data['zscores']
+    return model, sub_data
 
 if __name__ == '__main__':
     summary_output = 'output/simulation/single_causal_variant/pve_0.2/ld_0.8/gene_ENSG00000262000.1/pairwise_summary/pairs_summary'
@@ -47,11 +60,8 @@ if __name__ == '__main__':
 
         sub_summary_paths = [x for x in summary_model_paths if key in x]
         for summary_model_path in sub_summary_paths:
-            model, t1, t2 = load_model(data, summary_model_path=summary_model_path)
-            df = make_table(model, data)
-
-            import pdb; pdb.set_trace()
-            #df = df.loc[df.tissue in [t1, t2]]
+            model, sub_data = load_model(data, summary_model_path=summary_model_path)
+            df = make_table(model, sub_data)
             pairs = pair_coloc(df.loc[df.active == 1])
             if pairs.size > 0:
                 summary_pairs.append(pairs)
