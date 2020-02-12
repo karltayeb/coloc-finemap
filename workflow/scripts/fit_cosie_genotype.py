8e018e72b2842d31f0c662ac72d59059f7b77c18
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-11 21:18:08 -0500

    b

--- workflow/scripts/fit_cosie_genotype.py
+++ workflow/scripts/fit_cosie_genotype.py
@@ -2,13 +2,14 @@ import pickle
 from coloc.independent_model import IndependentFactorSER
 
 data = pickle.load(open(snakemake.input[0], 'rb'))
+
 Y = data['Y']
 if 't1' in snakemake.output[0]:
     t1 = int(snakemake.wildcards.tissue1)
     t2 = int(snakemake.wildcards.tissue2)
     Y = Y[[t1, t2]]
 
-g = IndependentFactorSER(X=data['X'], K=10)
+g = IndependentFactorSER(X=data['X'], Y=Y, K=10)
 g.fit(max_iter=100, update_active=False, update_weights=True, update_pi=True, 
       ARD_weights=True, update_variance=True, verbose=True)
 path = '/'.join(snakemake.output[0].split('/')[:-1])
