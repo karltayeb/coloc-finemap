6264c658e4ada2338d72cfd89528ba4db53470d4
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-16 18:30:51 -0500

    b

--- workflow/scripts/cafeh_plots.py
+++ workflow/scripts/cafeh_plots.py
@@ -6,7 +6,8 @@ def assign(obj, dictionary):
     for key in dictionary.keys():
         obj.__dict__[key] = dictionary[key]
 
-model_path = snakemake.input[0]
+model_path = snakemake.input.model_path
+data_path = snakemake.input.data_path
 component_plot_path = snakemake.output.component_plot_path
 zscore_plot_path = snakemake.output.zscore_plot_path
 
@@ -15,7 +16,7 @@ cafeh = MVNFactorSER(np.zeros((1, 1)), np.zeros((1, 1)), 1)
 
 assign(cafeh, pickle.load(open(model_path, 'rb')))
 path = '/'.join(model_path.split('/')[:-1])
-data = pickle.load(open('{}/data'.format(path), 'rb'))
+data = pickle.load(open(data_path, 'rb'))
 cafeh.X = data['LD']
 cafeh.Y = data['zscores']
 
