7485ef6d0d4ad9529042cfe7879b6d1367aefed1
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-16 16:03:59 -0500

    add genotype models to chr22 rule

--- workflow/scripts/fit_cafeh_genotype.py
+++ workflow/scripts/fit_cafeh_genotype.py
@@ -10,13 +10,12 @@ if 't1' in snakemake.output[0]:
     Y = Y[[t1, t2]]
 
 kwargs = {
-    'covariates': data['covariates'],
-    'snp_ids': data['variant_ids'],
-    'tissue_ids': data['tissue_ids'],
-    'sample_ids': data['sample_ids']
+    'covariates': data['covariates'] if 'covariates' in data else None,
+    'snp_ids': data['variant_ids'] if 'variant_ids' in data else None,
+    'tissue_ids': data['tissue_ids'] if 'tissue_ids' in data else None,
+    'sample_ids': data['sample_ids'] if 'sample_ids' in data else None
 }
 
-import pdb; pdb.set_trace()
 g = IndependentFactorSER(X=data['X'], Y=Y, K=10, **kwargs)
 g.fit(max_iter=100, update_active=False, update_weights=True, update_pi=True, 
       ARD_weights=True, update_variance=True, verbose=True)
