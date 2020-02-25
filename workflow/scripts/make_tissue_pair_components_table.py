1a85756f5defc4be39d05e9342f4db48416f4126
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-25 13:18:06 -0500

    b

--- workflow/scripts/make_tissue_pair_components_table.py
+++ workflow/scripts/make_tissue_pair_components_table.py
@@ -2,35 +2,25 @@ import pickle
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
-from coloc.ard_ser import MVNFactorSER
-from coloc.independent_model import IndependentFactorSER
+from coloc.cafeh import CAFEH
 from utils import *
 
-def load_models_and_data(data_path, genotype_model_path, summary_model_path):
+def load_models_and_data(data_path, summary_model_path):
     """
     load models and data
     """
-    genotype_model = IndependentFactorSER(np.zeros((1, 1)), np.zeros((1, 1)), 1)
-    summary_model = MVNFactorSER(np.zeros((1, 1)), np.zeros((1, 1)), 1)
-
+    summary_model = CAFEH(np.zeros((1, 1)), np.zeros((1, 1)), 1)
     data = pickle.load(open(data_path, 'rb'))
-    assign(genotype_model, pickle.load(open(genotype_model_path, 'rb')))
     assign(summary_model, pickle.load(open(summary_model_path, 'rb')))
-
-    genotype_model.X = data['X']
-    genotype_model.Y = data['Y']
 
     summary_model.X = data['LD']
     summary_model.Y = data['zscores']
-    return genotype_model, summary_model, data
+    return summary_model, data
 
+s, data = load_models_and_data(
+    snakemake.input.data_path, snakemake.input.summary_model_path)
 
-g, s, data = load_models_and_data(
-    snakemake.input.data_path, snakemake.input.genotype_model_path, snakemake.input.summary_model_path)
-df = make_table(g, data)
-pairs = pair_coloc(df, data)
-pairs.to_csv(snakemake.output.genotype_output, index=False, sep='\t')
-
+import pdb; pdb.set_trace()
 df = make_table(s, data)
 pairs = pair_coloc(df, data)
 pairs.to_csv(snakemake.output.summary_output, index=False, sep='\t')
