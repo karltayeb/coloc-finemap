f1057ddbbe78c5095695199493220f4e70bb580c
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-26 14:16:53 -0500

    b

--- workflow/scripts/make_maf_tss_table.py
+++ workflow/scripts/make_maf_tss_table.py
@@ -19,24 +19,23 @@ gene_id = g.apply(lambda x: x.values[8].split(';')[0].split('"')[1], axis=1)
 gene_info = pd.concat([g.iloc[:, 0], tss, gene_id], keys=['chromosome', 'tss', 'gene'], axis=1)
 gene_info_dict = {'chr{}'.format(k): gene_info.loc[gene_info.chromosome == 'chr{}'.format(k)].sort_values('tss') for k in range(1, 23)}
 
-outfile = open(snakemake.output[0], 'w')
-f = open(snakemake.input[0], 'r')
-f.readline()
-for line in f:
-    _, variant_id, ref, alt, alt_freq, obs_ct = f.readline().split('\t')
-    chrom, pos = variant_id.split('_')[0], int(variant_id.split('_')[1])
-    alt_freq = float(alt_freq)
-    maf = np.min([1 - alt_freq, alt_freq])
-    low = find(gene_info_dict[chrom].tss.values - pos, -500000, 0,
-               gene_info_dict[chrom].tss.values.size)
-    high = find(gene_info_dict[chrom].tss.values - pos, 500000, 0,
-                gene_info_dict[chrom].tss.values.size)
-    gene_info_dict[chrom].iloc[low:high].apply(
-        lambda x: print('{}\t{}\t{}\t{:.5f}\t{}'.format(
-            chrom, variant_id, x.gene, maf, np.abs(pos - x.tss)), file=outfile), axis=1
-    )
-    i += 1
-    if i % 1000 == 0:
-        print(i)
-f.close()
-outfile.close()
+i=0
+with open(snakemake.output[0], 'w') as g:
+    with open(snakemake.input[0], 'r') as f:
+        f.readline()
+        for line in f:
+            _, variant_id, ref, alt, alt_freq, obs_ct = f.readline().split('\t')
+            chrom, pos = variant_id.split('_')[0], int(variant_id.split('_')[1])
+            alt_freq = float(alt_freq)
+            maf = np.min([1 - alt_freq, alt_freq])
+            low = find(gene_info_dict[chrom].tss.values - pos, -500000, 0,
+                       gene_info_dict[chrom].tss.values.size)
+            high = find(gene_info_dict[chrom].tss.values - pos, 500000, 0,
+                        gene_info_dict[chrom].tss.values.size)
+            gene_info_dict[chrom].iloc[low:high].apply(
+                lambda x: print('{}\t{}\t{}\t{:.5f}\t{}'.format(
+                    chrom, variant_id, x.gene, maf, np.abs(pos - x.tss)), file=g), axis=1
+            )
+            i += 1
+            if i % 1000 == 0:
+                print(i)
