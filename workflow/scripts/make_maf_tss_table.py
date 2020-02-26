d209f59a8558e1ed9d5d311e7c797877800518ee
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-26 14:12:59 -0500

    b

--- workflow/scripts/make_maf_tss_table.py
+++ workflow/scripts/make_maf_tss_table.py
@@ -1,16 +1,26 @@
 import numpy as np
 import pandas as pd
-
+def find(arr, target, low, high):
+    if high - low <= 1:
+        return high
+    
+    mid = int((low + high) / 2)
+    if arr[mid] > target:
+        return find(arr, target, low, mid)
+    else:
+        return find(arr, target, mid, high)
+        
 gencode = pd.read_csv(('/work-zfs/abattle4/lab_data/GTEx_v8/references/'
                       'gencode.v26.GRCh38.genes.gtf'), sep='\t', skiprows=6, header=None)
 
 g = gencode[gencode.iloc[:, 2] =='gene']
 tss = g.apply(lambda x: x.values[3] if x.values[6] is '+' else x.values[4], axis=1)
 gene_id = g.apply(lambda x: x.values[8].split(';')[0].split('"')[1], axis=1)
+gene_info = pd.concat([g.iloc[:, 0], tss, gene_id], keys=['chromosome', 'tss', 'gene'], axis=1)
 gene_info_dict = {'chr{}'.format(k): gene_info.loc[gene_info.chromosome == 'chr{}'.format(k)].sort_values('tss') for k in range(1, 23)}
 
-outfile = with open(input[0], 'w')
-f = with open(output[0], 'r')
+outfile = open(snakemake.input[0], 'w')
+f = open(snakemake.output[0], 'r')
 f.readline()
 for line in f:
     _, variant_id, ref, alt, alt_freq, obs_ct = f.readline().split('\t')
