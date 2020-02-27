428702daa1c728aae33ff0edd5288701ffccebdb
Karl Tayeb <karl.tayeb7@gmail.com>
2020-02-27 12:22:38 -0500

    b

--- workflow/snk/data_prep.snk.py
+++ workflow/snk/data_prep.snk.py
@@ -20,9 +20,7 @@ rule grep_associations:
     output:
         'output/GTEx/gene_{gene}/associations/{tissue}.associations'
     run:
-        'grep {gene} \
-        /work-zfs/abattle4/lab_data/GTEx_v8/ciseQTL/GTEx_Analysis_v8_eQTL_all_associations/{tissue}.allpairs.txt \
-        > {output}'
+        'grep {gene} /work-zfs/abattle4/lab_data/GTEx_v8/ciseQTL/GTEx_Analysis_v8_eQTL_all_associations/{tissue}.allpairs.txt > {output}'
 
 rule get_tissue_specific_cov:
     input:
