31383efaa4bf030a5804a87a8a7ebf27ef27f647
Karl Tayeb <karl.tayeb7@gmail.com>
2020-03-02 18:07:35 -0500

    b

--- workflow/snk/data_prep.snk.py
+++ workflow/snk/data_prep.snk.py
@@ -172,9 +172,9 @@ rule make_gene_variant_lookup:
 
 rule get_tissue_specific_cov:
     input:
-        associations = 'output/GTEx/gene_{gene}/{gene}.associations',
-        ld = 'output/GTEx/gene_{gene}/{gene}.ld',
-        snps = 'output/GTEx/gene_{gene}/{gene}.snplist'
+        associations = 'output/{path}/{gene}.associations',
+        ld = 'output/{path}/{gene}.ld',
+        snps = 'output/{path}/{gene}.snplist'
     output:
         "output/{path}/tissue_specific_cov/data"
     params:
