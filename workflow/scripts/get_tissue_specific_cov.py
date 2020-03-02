5c988f4a2e3422fbd29cdbd85a534df143c76c3c
Karl Tayeb <karl.tayeb7@gmail.com>
2020-03-02 18:05:07 -0500

    b

--- workflow/scripts/get_tissue_specific_cov.py
+++ workflow/scripts/get_tissue_specific_cov.py
@@ -1,15 +1,36 @@
 import pickle
+import pandas as pd
 import numpy as np
+import os
 
-data = pickle.load(open(snakemake.input[0], 'rb'))
-alpha = snakemake.params.alpha
-if alpha is None:
-    alpha = 0.05
+# load data
+ld = pd.read_csv(snakemake.input.ld, sep='\t', header=None).values
+associations = pd.read_csv(snakemake.input.associations, sep='\t', index_col=0)
+snplist = np.squeeze(pd.read_csv(snakemake.input.snps, header=None).values)
 
-print('Making tissue specific covariance with alpha={}'.format(alpha))
+# filter out nans
+associations = associations.loc[:, ~np.any(np.isnan(associations), axis=0)]
+mask = ~np.any(np.isnan(ld), axis=1)
+ld = ld[mask][:, mask]
+snplist = snplist[mask]
+
+# filter to intersection
+intersect = np.intersect1d(snplist, associations.columns.values)
+associations = associations.loc[:, intersect]
+mask = np.isin(snplist, intersect)
+ld = ld[mask][:, mask]
+snplist = snplist[mask]
+
+alpha = 0.05
 LD = np.array([
-    (1 - alpha) * data['LD'] + alpha * np.outer(y, y) for y in data['Y']
+    (1 - alpha) * data['X'] + alpha * np.outer(y, y) for y in data['Y']
 ])
 
-data['LD'] = LD
+data = {
+    'X': LD,
+    'Y': associations.values,
+    'tissue_ids': associations.index.values,
+    'snp_ids': associations.columns.values
+}
+
 pickle.dump(data, open(snakemake.output[0], 'wb'))
