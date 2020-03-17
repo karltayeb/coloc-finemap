c4ae4879cdbf06866d49dd237dc755b3392f14c5
Karl Tayeb <karl.tayeb7@gmail.com>
2020-03-17 19:32:47 -0400

    b

--- workflow/scripts/get_global_regularized_cov.py
+++ workflow/scripts/get_global_regularized_cov.py
@@ -4,11 +4,6 @@ import numpy as np
 data = pickle.load(open(snakemake.input[0], 'rb'))
 #alpha = snakemake.params.alpha
 alpha = 0.1
-"""
-LD = np.array([
-    (1 - alpha) * data['LD'] + alpha * np.outer(y, y) for y in data['zscores']
-])
-"""
 zscore_cov = np.cov(data['Y'].T)
 data['X'] = (1 - alpha) * data['X'] + alpha * zscore_cov
 pickle.dump(data, open(snakemake.output[0], 'wb'))
