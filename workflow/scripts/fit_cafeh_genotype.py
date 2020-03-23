621213317721894e5c99f2d5572402d6e1997206
Karl Tayeb <karl.tayeb7@gmail.com>
2020-03-22 21:25:51 -0400

    save compact model

--- workflow/scripts/fit_cafeh_genotype.py
+++ workflow/scripts/fit_cafeh_genotype.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 from coloc.independent_model import IndependentFactorSER
 
 """
@@ -26,8 +27,31 @@ g.save(path, name)
 """
 data = pickle.load(open(snakemake.input[0], 'rb'))
 model = IndependentFactorSER(**data, K=20)
-model.fit(max_iter=300, verbose=True, ARD_weights=True)
+model.fit(max_iter=500, verbose=True, ARD_weights=True)
 
-path = '/'.join(snakemake.output[0].split('/')[:-1])
-name = snakemake.output[0].split('/')[-1]
-model.save(path, name)
+# get broad cs-- contains most information for model
+cs, p = model.get_credible_sets(0.999)
+active = np.array([p[k] > 0.01 for k in range(20)])
+
+# boolean mask for relevant snps
+snps_in_cs = np.unique(np.concatenate([cs[k] for k in range(20) if active[k]]))
+snps_in_cs = np.isin(model.snp_ids, snps_in_cs)
+
+# only retain relevant snps
+wm = model.weight_means[:, :, snps_in_cs]
+wv = model.weight_vars[:, :, snps_in_cs]
+pi = model.pi[:, snps_in_cs]
+
+# make save dict
+save_dict = model.__dict__
+save_dict.pop('X')
+save_dict.pop('Y')
+save_dict.pop('covariates')
+save_dict.pop('sample_covariate_map')
+
+save_dict['weight_means'] = wm
+save_dict['weight_vars'] = wv
+save_dict['snps_in_cs'] = snps_in_cs
+
+# save pickle
+pickle.dump(save_dict, open(snakemake.output[0], 'wb'))
