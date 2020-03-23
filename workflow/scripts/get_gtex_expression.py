a333f5163f55a28e742668aca4f4b4a513a89719
Karl Tayeb <karl.tayeb7@gmail.com>
2020-03-23 13:53:20 -0400

    add genotype summary output script

--- workflow/scripts/get_gtex_expression.py
+++ workflow/scripts/get_gtex_expression.py
@@ -22,6 +22,19 @@ for tissue in gene_tissue_map[test_gene]:
                              + '/{}.v8.normalized_expression.bed.gz'.format(tissue), sep='\t')
     gene_expression_df[tissue] = expression.loc[expression.gene_id == test_gene]
 
+"""
+dfs = {}
+paths = glob.glob('/work-zfs/abattle4/lab_data/GTEx_v8/ciseQTL/GTEx_Analysis_v8_eQTL_expression_matrices/*.bed.gz')
+
+for name, group in all_expression[all_expression.gene_id.isin(protein_coding.gene)].groupby(['#chr', 'gene_id']):
+    chrom, gene = name
+    print(gene)
+    group.index = group.index.droplevel(1)
+    group = group.iloc[:, 1:-3]
+    group = group.loc[:, ~np.all(np.isnan(group), axis=0)]
+    os.makedirs('output/GTEx/{}/{}'.format(chrom, gene))
+    group.to_csv('output/GTEx/{}/{}/{}.expression'.format(chrom, gene, gene), sep='\t')
+"""
 gene_expression = pd.concat(gene_expression_df, sort=False).droplevel(1)
 gene_expression = gene_expression.iloc[:, 4:]
 gene_expression = gene_expression.loc[:, ~np.all(np.isnan(gene_expression), 0)]
---
+++ workflow/scripts/report_genotype.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+import pickle
+from coloc.independent_model import IndependentFactorSER
+
+import os
+import glob
+
+import matplotlib.pyplot as plt
+
+def load_compact_model(path, gene):
+    #get data
+    model_dict = pickle.load(open(
+        '{}/genotype.model'.format(path), 'rb'))
+
+    genotype = pd.read_csv('{}/{}.raw'.format(path, gene), sep=' ', usecols=model_dict['snp_ids'][model_dict['snps_in_cs']])
+    expression = pd.read_csv('{}/{}.expression'.format(path, gene), sep='\t', index_col=0)
+    data = {
+        'X': genotype.values.T,
+        'Y': expression.values
+    }
+
+    # load model
+    model = IndependentFactorSER(**data, K=20)
+    model_dict.pop('dims')
+    model_dict['pi'] = model_dict['pi'][:, model_dict['snps_in_cs']]
+    model_dict['snp_ids'] = model_dict['snp_ids'][model_dict['snps_in_cs']]
+    model_dict['prior_pi'] = model_dict['prior_pi'][model_dict['snps_in_cs']]
+    model.__dict__.update(model_dict)
+    
+
+    return model
+
+def report_expected_weights(model, path, gene):
+    active = np.array([model.purity[k] > 0.1 for k in range(20)])
+    weights = pd.DataFrame(
+        model.get_expected_weights()[:, active],
+        index = model.tissue_ids,
+        columns = np.arange(20)[active]
+    )
+
+    weight_json = weights.to_json()
+    with open('{}/genotype.expected_weights'.format(path), 'w') as f:
+        f.write(weight_json)
+        
+        
+def report_ard_precision(model, path, gene):
+    active = np.array([model.purity[k] > 0.1 for k in range(20)])
+    weights = pd.DataFrame(
+        model.prior_precision[:, active],
+        index = model.tissue_ids,
+        columns = np.arange(20)[active]
+    )
+
+    weight_json = weights.to_json()
+    with open('{}/genotype.ard_precision'.format(path), 'w') as f:
+        f.write(weight_json)
+        
+def report_ard_precision(model, path, gene):
+    active = np.array([model.purity[k] > 0.1 for k in range(20)])
+    weights = pd.DataFrame(
+        model.prior_precision[:, active],
+        index = model.tissue_ids,
+        columns = np.arange(20)[active]
+    )
+
+    weight_json = weights.to_json()
+    with open('{}/genotype.ard_precision'.format(path), 'w') as f:
+        f.write(weight_json)
+        
+def report_credible_set(model, path, gene):
+    active = np.array([model.purity[k] > 0.1 for k in range(20)])
+    pi = pd.DataFrame(model.pi.T, index=model.snp_ids)
+    min_cset_alpha = pd.concat(
+        [pi.iloc[:, k].sort_values(ascending=False).cumsum() for k in np.arange(20)[active]],
+        sort=False, axis=1
+    ).min(1)
+
+    with open('{}/genotype.cset.bed'.format(path), 'w') as f:
+        for row in min_cset_alpha.reset_index().values:
+            variant, val = row
+            chrom, pos = variant.split('_')[:2]
+            pos = int(pos)
+            line = '{}\t{}\t{}\t{}\t{}'.format(chrom, pos, pos+1, gene, val)
+            print(line, file=f)
+
+paths = glob.glob('../../output/GTEx/*/*/genotype.model')
+
+for path in paths:
+    path =  '/'.join(path.split('/')[:-1])
+    gene = path.split('/')[-1]
+    print(gene)
+    
+    model = load_compact_model(path, gene)
+    report_credible_set(model, path, gene)
+    report_expected_weights(model, path, gene)
+    report_ard_precision(model, path, gene)
