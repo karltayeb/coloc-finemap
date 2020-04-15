59c907f9aa48060135ec1dca21be7ba7796beb65
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-14 20:39:03 -0400

    clean up data dict file

--- workflow/scripts/get_gtex_genotype_model_data.py
+++ workflow/scripts/get_gtex_genotype_model_data.py
@@ -2,46 +2,56 @@ import pandas as pd
 import numpy as np
 import pickle
 
-# load expression
-gene_expression = pd.read_csv(snakemake.input.expression, sep='\t', index_col=0)
+def make_gtex_genotype_data_dict(expression_path, genotype_path, save_path, params):
+    # load expression
+    gene_expression = pd.read_csv(expression_path, sep='\t', index_col=0)
 
-#load genotype
-genotype = pd.read_csv(snakemake.input.genotype, sep=' ')
-genotype = genotype.set_index('IID').iloc[:, 5:]
+    #load genotype
+    genotype = pd.read_csv(genotype_path, sep=' ')
+    genotype = genotype.set_index('IID').iloc[:, 5:]
 
-# center, mean immpute
-genotype = (genotype - genotype.mean(0))
-genotype = genotype.fillna(0)
+    # center, mean immpute
+    genotype = (genotype - genotype.mean(0))
+    genotype = genotype.fillna(0)
 
-# standardize
-if snakemake.params.standardize == True:
-    genotype = genotype / genotype.std(0)
+    # standardize
+    if params.standardize == True:
+        genotype = genotype / genotype.std(0)
 
-# drop individuals that do not have recorded expression
-gene_expression = gene_expression.loc[:, ~np.all(np.isnan(gene_expression), 0)]
+    # drop individuals that do not have recorded expression
+    gene_expression = gene_expression.loc[:, ~np.all(np.isnan(gene_expression), 0)]
 
-# filter down to relevant individuals
-genotype = genotype.loc[gene_expression.columns]
+    # filter down to relevant individuals
+    genotype = genotype.loc[gene_expression.columns]
 
-# filter down to common individuals
-individuals = np.intersect1d(genotype.index.values, gene_expression.columns.values)
-genotype = genotype.loc[individuals]
-gene_expression = gene_expression.loc[:, individuals]
+    # filter down to common individuals
+    individuals = np.intersect1d(genotype.index.values, gene_expression.columns.values)
+    genotype = genotype.loc[individuals]
+    gene_expression = gene_expression.loc[:, individuals]
 
-# load covariates
-covariates = pd.read_csv('../../output/GTEx/covariates.csv', sep='\t', index_col=[0, 1])
-covariates = covariates.loc[gene_expression.index]
-covariates = covariates.loc[:, genotype.index.values]
+    # load covariates
+    covariates = pd.read_csv('../../output/GTEx/covariates.csv', sep='\t', index_col=[0, 1])
+    covariates = covariates.loc[gene_expression.index]
+    covariates = covariates.loc[:, genotype.index.values]
 
-X = genotype.values.T
+    X = genotype.values.T
 
-data = {
-    'X': X,
-    'Y': gene_expression.values,
-    'covariates': covariates,
-    'snp_ids': genotype.columns.values,
-    'sample_ids': genotype.index.values,
-    'tissue_ids': gene_expression.index.values
-}
+    data = {
+        'X': X,
+        'Y': gene_expression.values,
+        'covariates': covariates,
+        'snp_ids': genotype.columns.values,
+        'sample_ids': genotype.index.values,
+        'tissue_ids': gene_expression.index.values
+    }
 
-pickle.dump(data, open(snakemake.output[0], 'wb'))
+    pickle.dump(data, open(save_path, 'wb'))
+
+
+make_gtex_genotype_data_dict(
+    snakemake.input.expression,
+    snakemake.input.genotype,
+    snakemake.output[0],
+    snakemake.params
+)
+
