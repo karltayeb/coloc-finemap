f4f643481b03233319aac24463e44f2e4a98fed1
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-21 10:09:52 -0400

    b

--- workflow/snk/caviar.snk.py
+++ workflow/snk/caviar.snk.py
@@ -7,7 +7,7 @@ rule format_caviar_data_ld:
         '{path}/{prefix}.data'
     output:
         ld_matrix = temp('{path}/caviar/{prefix}.data.ld'),
-        zscores = temp(enumerate('output/{path}/caviar/{prefix}.data.zscores{tissue}', t=range(20)))
+        zscores = temp(expand('output/{path}/caviar/{prefix}.data.zscores{t}', t=list(range(20))))
 
     run:
         data = pickle.load(open(input[0], 'rb'))
