a482156eb26a89f64618b40ca502468d62468033
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-21 15:28:22 -0400

    b

--- workflow/scripts/run_coloc.R
+++ workflow/scripts/run_coloc.R
@@ -21,12 +21,12 @@ for (t1 in c(1:num_tissues)){
 		i <- i + 1
 		beta1 = data$zscores[t1,]
 		beta2 = data$zscores[t2,]
-		v1 = replicate(length(beta1), data$standard_errors[t1]^2)
-		v2 = replicate(length(beta2), data$standard_errors[t2]^2)
+		v1 = data$standard_errors[t2,]^2
+		v2 = data$standard_errors[t2,]^2
 		res = coloc.abf(
-			dataset1=list(beta=beta1, varbeta=v1, N=length(beta1), sdY=sd(data$Y[t1,]), type='quant'),
-			dataset2=list(beta=beta2, varbeta=v2, N=length(beta2), sdY=sd(data$Y[t2,]), type='quant')
-			)
+			dataset1=list(beta=beta1, varbeta=v1, N=838, sdY=sd(data$Y[t1,]), type='quant'),
+			dataset2=list(beta=beta2, varbeta=v2, N=838, sdY=sd(data$Y[t2,]), type='quant')
+		)
 		table$t1[i] <- t1
 		table$t2[i] <- t2
 		table$PPH0[i] <- res$summary[2]
--- workflow/snk/coloc.snk.py
+++ workflow/snk/coloc.snk.py
@@ -1,3 +1,27 @@
+rule format_coloc_data:
+    input:
+        '{path}/{prefix}.data'
+    output:
+        coloc_data = temp('{path}/caviar/{prefix}.colc.data'),
+    wildcard_constraints:
+        prefix='[^/]+'
+    run:
+        data = pickle.load(open(input[0], 'rb'))
+        print('computing summary stats')
+        X = data['X']
+        Y = data['Y']
+        n = Y.shape[1]
+        xx = np.einsum('nm,nm->n', X, X)
+        B = Y@X.T / xx
+        S2 = np.sum((Y[:, None] - B[..., None] * X)**2, 2) / (xx * (n-2))
+        Z = B / np.sqrt(S2)
+        data = {
+        	'Y': Y
+        	'zscores': Z,
+        	'standard_errors': np.sqrt(S2)
+        }
+        pickle.dump(data, open(output.coloc_data, 'rb'))
+
 rule run_coloc:
     input:
         "output/simulation/{simulation}/{settings}/gene_{gene}/data"
@@ -5,6 +29,16 @@ rule run_coloc:
         "output/simulation/{simulation}/{settings}/gene_{gene}/coloc"
     wildcard_constraints:
         simulation = "(?!\/)[^\/]+(?=\/)"
+    script:
+        "../../workflow/scripts/run_coloc.R"
+
+rule run_coloc:
+    input:
+        "{path}/{prefix}.coloc.data"
+    output:
+        "{output}/{prefix}.coloc}"
+    wildcard_constraints:
+        prefix='[^/]+'
     script:
         "../../workflow/scripts/run_coloc.R"
 
