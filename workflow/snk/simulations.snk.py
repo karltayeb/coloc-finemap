ab610bdc251cc9ae82873485530439cde3249d3a
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-22 00:03:01 -0400

    b

--- workflow/snk/simulations.snk.py
+++ workflow/snk/simulations.snk.py
@@ -34,14 +34,14 @@ rule simulate_single:
         expand("output/sim/single/{chr_gene}/genotype.sim.t10.pve10.coloc", chr_gene=chr_gene),
         expand("output/sim/single/{chr_gene}/genotype.sim.t10.pve20.model", chr_gene=chr_gene),
         expand("output/sim/single/{chr_gene}/genotype.sim.t10.pve20.coloc", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve01.model", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve01.coloc", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve05.model", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve05.coloc", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve10.model", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve10.coloc", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve20.model", chr_gene=chr_gene),
-        expand("output/sim/multiple/{chr_gene}/genotype.sim.t10.pve20.coloc", chr_gene=chr_gene)
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve01.model", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve01.coloc", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve05.model", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve05.coloc", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve10.model", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve10.coloc", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve20.model", chr_gene=chr_gene),
+        expand("output/sim/multiple/{chr_gene}/genotype.sim.t20.pve20.coloc", chr_gene=chr_gene)
 
 rule simulate_single_causal_variant2:
     input:
