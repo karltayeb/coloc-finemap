c3c44e8435270c11a82ee7a2a76e8cc505ecf982
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-22 08:39:15 -0400

    b

--- workflow/scripts/make_ecaviar_table.py
+++ workflow/scripts/make_ecaviar_table.py
@@ -4,10 +4,6 @@ import pandas as pd
 import numpy as np
 import glob
 
-data = pickle.load(
-    open(snakemake.input.data, 'rb')
-)
-
 dfs = []
 key = []
 for p in snakemake.input.caviar_posteriors:
@@ -17,15 +13,12 @@ for p in snakemake.input.caviar_posteriors:
 df = pd.concat(dfs, keys=key, axis=1)
 
 table = []
-labels = (data['true_effects'] @ data['true_effects'].T > 0)
-
 for t1, t2 in itertools.combinations(np.arange(len(key)), 2):
     clpp = (df.loc[:, 't{}'.format(t1)] * df.loc[:, 't{}'.format(t2)]).max()[0]
     table.append({
         't1': t1,
         't2': t2,
-        'CLPP': clpp,
-        'label': labels[t1, t2]
+        'CLPP': clpp
     })
 table = pd.DataFrame(table)
 table.to_csv(snakemake.output[0])
