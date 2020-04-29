55a81cee81eb736d9e70198571e1fcc740a804dd
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-29 10:24:44 -0400

    b

--- workflow/snk/simulations.snk.py
+++ workflow/snk/simulations.snk.py
@@ -38,12 +38,23 @@ rule simulate_single:
         expand("output/sim/multiple_random/{chr_gene}/genotype.sim.t10.pve20.ecaviar", chr_gene=chr_gene)
 
 
+rule run_susie_on_simulations:
+    input:
+        data="output/sim/multiple/{chr}/{gene}/genotype.sim.t{t}.pve{pve}.data"
+    output:
+        susie="output/sim/multiple/{chr}/{gene}/genotype.sim.t{t}.pve{pve}.susie",
+    wildcard_constraints:
+        gene = "[^\/]+(?=\/)"
+    params:
+        sample_effects=False
+    script:
+        "../../workflow/scripts/sim_susie.py"
+
 rule simulate_multiple_causal_variant_fixed_effect_size:
     input:
         genotype="output/GTEx/{chr}/{gene}/{gene}.raw"
     output:
         model="output/sim/multiple/{chr}/{gene}/genotype.sim.t{t}.pve{pve}.model",
-        susie="output/sim/multiple/{chr}/{gene}/genotype.sim.t{t}.pve{pve}.susie",
         info="output/sim/multiple/{chr}/{gene}/sim.t{t}.pve{pve}.info",
         data="output/sim/multiple/{chr}/{gene}/genotype.sim.t{t}.pve{pve}.data"
     wildcard_constraints:
