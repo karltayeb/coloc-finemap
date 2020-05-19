e3aa97131f0d027e4492c0c11c7f99b7c4e6ad61
Karl Tayeb <karl.tayeb7@gmail.com>
2020-05-19 09:07:26 -0400

    b

--- workflow/snk/data_prep.snk.py
+++ workflow/snk/data_prep.snk.py
@@ -137,8 +137,8 @@ rule get_1kG_EUR_genotype:
         from_bp = lambda wildcards: np.maximum(0, gencode.loc[wildcards.gene].tss - 1000000),
         to_bp = lambda wildcards: gencode.loc[wildcards.gene].tss + 1000000
     output:
-        'output/GTEx/{chrom}/{gene}/{gene}.snplist',
-        'output/GTEx/{chrom}/{gene}/{gene}.raw'
+        'output/GTEx/{chrom}/{gene}/{gene}.1kG.hg38.EUR.snplist',
+        'output/GTEx/{chrom}/{gene}/{gene}.1kG.hg38.EUR.raw'
     shell:
         'plink --bfile /work-zfs/abattle4/marios/annotations/1kG_plink/1kG_hg38_ERU'
         ' --chr {params.chrom} --from-bp {params.from_bp} --to-bp {params.to_bp}  --maf 0.01  --geno 0.1'
