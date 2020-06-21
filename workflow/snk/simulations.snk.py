6cad915f0689066c00b1cbe2cd1e210b21d4581c
Karl Tayeb <karl.tayeb7@gmail.com>
2020-06-21 18:09:30 -0400

    large_n_study sim

--- workflow/snk/simulations.snk.py
+++ workflow/snk/simulations.snk.py
@@ -105,6 +105,14 @@ rule run_sim_n_causal:
     input:
         expand('{path}', path=log_files)
 
+
+sim_spec = pd.read_csv('output/sim/large_n_study/sim_spec.txt', sep='\t', index_col=0)
+log_files = sim_spec.log_path.values
+log_files = [x[len('/work-zfs/abattle4/karl/cosie_analysis/'):] for x in log_files]
+rule run_sim_large_n_study:
+    input:
+        expand('{path}', path=log_files)
+
 rule score_sim:
     input:
         'output/sim/{sim}/sim_spec.txt',
