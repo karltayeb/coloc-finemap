cfddfc7515db8d395ddc7193b840bbb87be2fa8e
karltayeb <karl.tayeb7@gmail.com>
2021-12-06 12:42:15 -0600

    gwas only workflow

---
+++ workflow/snk/gwas_only.snk.py
@@ -0,0 +1,72 @@
+rule get_gtex_genotype_for_gwas:
+    output:
+        snplist = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.snplist',
+        genotype = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.raw',
+        log = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.log'
+    group: "g"
+    run:
+        from utils.misc import plink_get_genotype
+        import subprocess
+
+        def load_lookup():
+            df = pd.read_csv(
+                '/work-zfs/abattle4/marios/GTEx_v8/coloc/aggregate_phenotype_loci', sep='\t', header=None)
+            df.columns = ['chrom', 'start', 'end', 'rsid', 'phenotype']
+            df = df.set_index(['chrom', 'rsid'])
+            return df
+
+        def plink_get_genotype_gwas_only(bfile, save_path):
+            chrom = path.split('/')[-3]
+            rsid = path.split('/')[-2]
+            start, end = lookup.loc[(chrom, rsid)].values[0][:2]
+
+            cmd = ' '.join(
+                ['plink',
+                 '--bfile', bfile,
+                 '--chr', chrom[3:],
+                 '--from-bp', str(start),
+                 '--to-bp', str(end),
+                 '--maf', '0.01',
+                 '--geno', '0.1',
+                 '--recode', 'A',
+                 '--keep-allele-order',
+                 '--snps-only', '--write-snplist', '--allow-no-sex',
+                 '--out', save_path])
+            return cmd
+
+        lookup = load_lookup()
+        gtex_bfile = '/work-zfs/abattle4/marios/GTEx_v8/coloc/GTEx_all_genotypes'
+        cmd = plink_get_genotype(lookup, gtex_bfile, output.genotype[:-4])
+        print(cmd)
+        #subprocess.run(cmd, shell=True)
+        shell(cmd)
+        print('PLINK FINISHED RUNNING?')
+
+rule snpid2rsid_for_gwas:
+    input:
+        'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.snplist'
+    output:
+       rsids = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.rsids',
+       rsid_map = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.snp2rsid'
+    group: "g"
+    script:
+        "../../workflow/scripts/variantid2rsid.py"
+
+
+rule fit_gwas_z_cafeh:
+    input:
+        genotype_gtex = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.raw',
+        sumstats='output/{study}/{phenotype}/{phenotype}.tsv.bgz',
+        tabix_index='output/{study}/{phenotype}/{phenotype}.tsv.bgz.tbi',
+        v2r = 'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.snp2rsid'
+    output:
+        variant_report=\
+            'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.z.variant_report'
+        model=\
+            'output/GWAS_only/{study}/{phenotype}/{chr}/{locus}/{phenotype}.{locus}.z.css'
+    params:
+        K=20,
+        zscore=True
+    group: 'report'
+    script:
+        '../../workflow/scripts/ukbb_cafeh_ss.py'
